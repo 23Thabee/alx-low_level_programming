0233c5c184040c138c1a4592c58783bffffd5573
23Thabee <mrethabile911@gmail.com>
2023-06-19 08:02:16 -0700

    print_alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 /**
 *main - Complete the source code in order to print the last digit
 *each time it is executed
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int n;
@@ -19,7 +19,8 @@ int main(void)
 
 	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 
-	else if ((n % 10) < 6 && (n % 10)); '!' == (0);
+	else if ((n % 10) < 6 && (n % 10) 
+	'!' == (0);
 
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 
